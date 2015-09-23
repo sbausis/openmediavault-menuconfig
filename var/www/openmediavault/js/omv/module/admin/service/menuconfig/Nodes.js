c7d38bb1755d8fb56f072c8d2d0a355392882022
sbausis <sbausis@gmx.net>
2015-09-23 22:57:48 +0200

    add new omv-menunodes / take fullpath as id / cleanup

--- var/www/openmediavault/js/omv/module/admin/service/menuconfig/Nodes.js
+++ var/www/openmediavault/js/omv/module/admin/service/menuconfig/Nodes.js
@@ -53,7 +53,7 @@ Ext.define("OMV.module.admin.service.menuconfig.Nodes", {
             store : Ext.create("OMV.data.Store", {
                 autoLoad : true,
                 model    : OMV.data.Model.createImplicit({
-                    idProperty  : "id",
+                    idProperty  : "fullpath",
                     fields      : [
 						{ name: "id", type: "string" },
 						{ name: "path", type: "string" },
@@ -74,130 +74,6 @@ Ext.define("OMV.module.admin.service.menuconfig.Nodes", {
         me.callParent(arguments);
     }
 });
-
-//Ext.define("OMV.module.admin.service.menuconfig.Nodes", {
-//	extend: "OMV.workspace.grid.Panel",
-//	requires: [
-//		"OMV.data.Store",
-//		"OMV.data.Model",
-//		"OMV.data.proxy.Rpc",
-//		"OMV.util.Format"
-//	],
-//
-//	stateful: true,
-//	stateId: "e03bdc0e-4f66-11e2-8510-00221568ca88",
-//	bodyCls: "x-grid-without-dirty-cell",
-//	features: [{
-//		ftype: "grouping",
-//		groupHeaderTpl: "{name}"
-//	}],
-//	columns: [{
-//		text: _("fullpath"),
-//		sortable: true,
-//		groupable: true,
-//		hidden: false,
-//		dataIndex: "fullpath",
-//		stateId: "fullpath",
-//		align: "center",
-//		renderer: function(value, metaData, record, rowIndex, colIndex,
-//		  store, view) {
-//			return _(value);
-//		}
-//	},{
-//		text: _("id"),
-//		sortable: true,
-//		groupable: false,
-//		dataIndex: "id",
-//		stateId: "id",
-//		flex: 1,
-//		renderer: function(value, metaData, record, rowIndex, colIndex,
-//		  store, view) {
-//			return _(value);
-//		}
-//	},{
-//		text: _("path"),
-//		sortable: true,
-//		groupable: false,
-//		dataIndex: "path",
-//		stateId: "path",
-//		flex: 1,
-//		renderer: function(value, metaData, record, rowIndex, colIndex,
-//		  store, view) {
-//			return _(value);
-//		}
-//	},{
-//		xtype: "checkcolumn",
-//		text: _("Enable"),
-//		groupable: false,
-//		dataIndex: "enable",
-//		stateId: "enable",
-//		align: "center",
-//		width: 100,
-//		resizable: false
-//	}],
-//	hideAddButton: true,
-//	hideEditButton: true,
-//	hideDeleteButton: true,
-//	hideApplyButton: false,
-//	hideRefreshButton: false,
-//
-//	initComponent: function() {
-//		var me = this;
-//		Ext.apply(me, {
-//			store: Ext.create("OMV.data.Store", {
-//				autoLoad: true,
-//				groupField: "fullpath",
-//				model: OMV.data.Model.createImplicit({
-//					idProperty: "id",
-//					fields: [
-//						{ name: "id", type: "string" },
-//						{ name: "path", type: "string" },
-//						{ name: "fullpath", type: "string" },
-//						{ name: "enable", type: "boolean" }
-//					]
-//				}),
-//				proxy: {
-//					type: "rpc",
-//					appendSortParams: true,
-//					rpcData: {
-//						service: "Menuconfig",
-//						method: "getNodes"
-//					}
-//				},
-//				sorters: [{
-//					direction: "ASC",
-//					property: "id"
-//				}]
-//			})
-//		});
-//		me.callParent(arguments);
-//	},
-//
-//	onApplyButton: function() {
-//		var me = this;
-//		var records = me.store.getRange();
-//		var params = [];
-//		//Ext.Array.each(records, function(record) {
-//		//	params.push({
-//		//		  "fullpath": record.get("fullpath"),
-//		//		  "enable": record.get("enable")
-//		//	  });
-//		//});
-//		//// Execute RPC.
-//		//OMV.Rpc.request({
-//		//	  scope: me,
-//		//	  callback: function(id, success, response) {
-//		//		  this.store.reload();
-//		//	  },
-//		//	  relayErrors: false,
-//		//	  rpcData: {
-//		//		  service: "Menuconfig",
-//		//		  method: "setNodes",
-//		//		  params: params
-//		//	  }
-//		//  });
-//	}
-//});
 
 OMV.WorkspaceManager.registerPanel({
     id        : "nodes",
